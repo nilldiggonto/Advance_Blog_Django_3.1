f525a1e51c286ff74a1109b7029d64b960f9bb3e
nilldiggonto <nilldiggonto@gmail.com>
2020-11-04 05:58:23 +0600

    Paginator

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from django.views.generic import ListView,DeleteView
 # Create your views here.
+
+################################## Function Based ###################################
 def post_list(request):
-    posts = Post.published.all()
+    
+    object_list = Post.published.all()
     template_name = 'post/list.html'
+    paginator = Paginator(object_list,2)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+
     context = {
+        'page':page,
         'posts':posts
     }
+
+
+
+
+
+
     return render(request,template_name,context)
 
 
