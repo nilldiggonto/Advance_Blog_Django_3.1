cbe031f76ebfe2ff05d02d74cbd1becedc4f467c
nilldiggonto <nilldiggonto@gmail.com>
2020-11-06 00:28:45 +0600

    RSS Feed

---
+++ blog/feeds.py
@@ -0,0 +1,18 @@
+from django.contrib.syndication.views import Feed
+from django.template.defaultfilters import truncatewords
+from django.urls import reverse_lazy
+from .models import Post
+
+class LatestPostsFeed(Feed):
+    title       = "Nill Blog"
+    link        = reverse_lazy('blog:post_list')
+    description = "New Posts has been created"
+
+    def items(self):
+        return Post.published.all()[:1]
+
+    def item_title(self,item):
+        return item.title
+
+    def item_description(self,item):
+        return truncatewords(item.body,30)
--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from  .views import post_list,post_detail, PostListView,post_share
 from .sitemaps import PostSitemap
+from .feeds import LatestPostsFeed
+
 app_name ='blog'
 
 urlpatterns = [
@@ -9,4 +11,5 @@ urlpatterns = [
     #path('',PostListView.as_view(),name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',post_detail,name='post_detail'),
     path('<int:post_id>/share/',post_share,name='post_share'),
+    path('feed/',LatestPostsFeed(),name='post_feed'),
 ]
